409ffe712c17de77a6a1e445c5c711698b8febbe
HystERICKal <nyoroerick12@gmail.com>
2021-07-23 14:29:44 +0200

    added share, like and comment buttons

--- switter/src/pages/PageHome.vue
+++ switter/src/pages/PageHome.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page>
+<q-page>
     <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
 
       <div class="col">
@@ -40,6 +40,66 @@
 
     <q-separator size="10px" color="grey-2" class="boarder-separeta"/>
 
+     <q-list>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <!-- <q-item-label><strong>Erick Francis</strong></q-item-label>
+          <span class="text-grey-7">
+            @erick__francis21
+          </span> -->
+          <q-item-label class="text-subtitle1"><strong>Confession #601</strong></q-item-label>
+          <q-item-label class="confess-content text-body1">Lorem ipsum, dolor sit amet consectetur adipisicing elit.            
+            
+            Quia animi necessitatibus recusandae in numquam, ea dignissimos 
+            voluptatum hic voluptate ipsum inventore. Quam error unde odit 
+            soluta sapiente veniam architecto culpa?
+          </q-item-label>
+          <div class="row justify-between q-mt-sm">
+          <q-btn 
+          flat 
+          round 
+          color="grey" 
+          icon="far fa-comment"
+          size="sm" />
+
+          <q-btn 
+          flat 
+          round 
+          color="grey" 
+          icon="fas fa-retweet"
+          size="sm" />
+
+          <q-btn 
+          flat 
+          round 
+          color="grey" 
+          icon="far fa-heart"
+          size="sm" />
+
+          <q-btn 
+          flat 
+          round 
+          color="grey" 
+          icon="fas fa-trash"
+          size="sm" />
+          </div>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+
+      
+      
+    </q-list>
+
   </q-page>
 </template>
 
@@ -62,9 +122,10 @@ export default defineComponent({
   textarea
     font-size: 19px
     line-height: 1.4 !important
-
 .boarder-separeta
   border-top: 1px solid 
   border-bottom: 1px solid 
-  border-color: $grey-2
+  border-color: $grey-4
+.confess-content
+  white-space: pre-line
 </style>
